6a20d5b0790d81759f253b81c68fbf6d685f0b31
Wafa AF <wafaabufaour@gmail.com>
2024-07-08 11:40:19 -0700

    Fortify

--- config/app.php
+++ config/app.php
@@ -122,5 +122,8 @@ return [
         'driver' => env('APP_MAINTENANCE_DRIVER', 'file'),
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
-
+    // 'providers' => [
+    //     // Other service providers...
+    //     App\Providers\FortifyServiceProvider::class,
+    // ],
 ];
